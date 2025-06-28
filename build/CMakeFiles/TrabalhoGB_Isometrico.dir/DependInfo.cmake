cd55db63f241130cd60c950ac3a487a1471d4a37
Kevin Garcia <kevin.a.g.97@hotmail.com>
2025-06-28 03:43:36 -0300

    Refactor code structure for improved readability and maintainability

---
+++ build/CMakeFiles/TrabalhoGB_Isometrico.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Kevin/OneDrive/Documentos/PGCCHIB/Common/glad.c" "CMakeFiles/TrabalhoGB_Isometrico.dir/Common/glad.c.obj" "gcc" "CMakeFiles/TrabalhoGB_Isometrico.dir/Common/glad.c.obj.d"
+  "C:/Users/Kevin/OneDrive/Documentos/PGCCHIB/src/GB/Game.cpp" "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Game.cpp.obj" "gcc" "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Game.cpp.obj.d"
+  "C:/Users/Kevin/OneDrive/Documentos/PGCCHIB/src/GB/Player.cpp" "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Player.cpp.obj" "gcc" "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Player.cpp.obj.d"
+  "C:/Users/Kevin/OneDrive/Documentos/PGCCHIB/src/GB/Renderer.cpp" "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Renderer.cpp.obj" "gcc" "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Renderer.cpp.obj.d"
+  "C:/Users/Kevin/OneDrive/Documentos/PGCCHIB/src/GB/Shader.cpp" "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Shader.cpp.obj" "gcc" "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Shader.cpp.obj.d"
+  "C:/Users/Kevin/OneDrive/Documentos/PGCCHIB/src/GB/Texture.cpp" "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Texture.cpp.obj" "gcc" "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Texture.cpp.obj.d"
+  "C:/Users/Kevin/OneDrive/Documentos/PGCCHIB/src/GB/Tilemap.cpp" "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Tilemap.cpp.obj" "gcc" "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Tilemap.cpp.obj.d"
+  "C:/Users/Kevin/OneDrive/Documentos/PGCCHIB/src/GB/main.cpp" "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/main.cpp.obj" "gcc" "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/main.cpp.obj.d"
+  "C:/Users/Kevin/OneDrive/Documentos/PGCCHIB/src/GB/stb_impl.cpp" "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/stb_impl.cpp.obj" "gcc" "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/stb_impl.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/TrabalhoGB_Isometrico.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/Common/glad.c.obj"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/Common/glad.c.obj.d"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Game.cpp.obj"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Game.cpp.obj.d"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Player.cpp.obj"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Player.cpp.obj.d"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Renderer.cpp.obj"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Renderer.cpp.obj.d"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Shader.cpp.obj"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Shader.cpp.obj.d"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Texture.cpp.obj"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Texture.cpp.obj.d"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Tilemap.cpp.obj"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/Tilemap.cpp.obj.d"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/main.cpp.obj"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/main.cpp.obj.d"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/stb_impl.cpp.obj"
+  "CMakeFiles/TrabalhoGB_Isometrico.dir/src/GB/stb_impl.cpp.obj.d"
+  "TrabalhoGB_Isometrico.exe"
+  "TrabalhoGB_Isometrico.exe.manifest"
+  "TrabalhoGB_Isometrico.pdb"
+  "libTrabalhoGB_Isometrico.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/TrabalhoGB_Isometrico.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
